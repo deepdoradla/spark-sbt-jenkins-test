4ab956ca39e7e7c7d7edfa0b9a8203bd1f1e3752
Deep Doradla <deepdoradla@Deeps-MacBook-Pro.local>
2017-06-25 21:11:40 +0100

    Added spark confs and imported the libs

--- src/main/scala-2.11/simplejob/SimpleJobApp.scala
+++ src/main/scala-2.11/simplejob/SimpleJobApp.scala
@@ -3,23 +3,20 @@ package simplejob
 /**
   * Created by deepdoradla on 24/06/2017.
   */
-object SimpleJobApp extends App{
 
-  override def main(args: Array[String]) {
-    //val sc = new SparkContext
-    //val sqlContext = new SQLContext(sc)
+import org.apache.spark._
+import org.apache.spark.sql._
 
+object SimpleJobApp {
+
+  def main(args: Array[String]){
+    val conf = new SparkConf().setAppName("Test Job Application")
+    val sc = new SparkContext(conf)
+    val sqlContext = new SQLContext(sc)
     println("I am executing")
-
-    val numA = 1
-    val numB = 2
-
-    println("Value of A is: " + numA + " Value of B is: " + numB)
-
   }
 
   def getA() : Int = {
-
     val valueA = 2
 
     return valueA
